0a956a895ca7f901f4b186d220f256ee6cad4d2c
Anna Kolganova <anechkaprokopas@gmail.com>
2022-04-05 00:14:32 +1000

    Add validation to add task screen

--- lib/AddTaskScreen.dart
+++ lib/AddTaskScreen.dart
@@ -20,75 +20,88 @@ class AddTaskScreenState extends State<AddTaskScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+
     return Scaffold(
         appBar: AppBar(
           title: const Text("Add task"),
         ),
-        body: Column(
-          children: [
-            TextField(
-              onChanged: (value) => {taskText = value},
-              decoration: const InputDecoration(
-                labelText: 'Task text',
-              ),
-            ),
-            RaisedButton(
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
-              elevation: 4.0,
-              onPressed: () {
-                DatePicker.showDateTimePicker(context,
-                    theme: const DatePickerTheme(
-                      containerHeight: 210.0,
-                    ),
-                    showTitleActions: true, onConfirm: (time) {
-                  taskTime = time;
-                  setState(() {});
-                }, currentTime: DateTime.now(), locale: LocaleType.en);
-                setState(() {});
-              },
-              child: Container(
-                alignment: Alignment.center,
-                height: 50.0,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Row(
+        body: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  initialValue: '',
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    setState(() => taskText = value!);
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Task text',
+                  ),
+                ),
+                RaisedButton(
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+                  elevation: 4.0,
+                  onPressed: () {
+                    DatePicker.showDateTimePicker(context,
+                        theme: const DatePickerTheme(
+                          containerHeight: 210.0,
+                        ),
+                        showTitleActions: true, onConfirm: (time) {
+                      taskTime = time;
+                      setState(() {});
+                    }, currentTime: DateTime.now(), locale: LocaleType.en);
+                    setState(() {});
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 50.0,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: <Widget>[
-                        Container(
-                          child: Row(
-                            children: <Widget>[
-                              const Icon(
-                                Icons.access_time,
-                                size: 18.0,
-                                color: Colors.blue,
+                        Row(
+                          children: <Widget>[
+                            Container(
+                              child: Row(
+                                children: <Widget>[
+                                  const Icon(
+                                    Icons.access_time,
+                                    size: 18.0,
+                                    color: Colors.blue,
+                                  ),
+                                  Text(
+                                    ' ' + (taskTime == null ? '' : DateFormat('yyyy-MM-dd HH:mm').format(taskTime!)),
+                                    style: const TextStyle(color: Colors.blue, fontWeight: FontWeight.bold, fontSize: 18.0),
+                                  ),
+                                ],
                               ),
-                              Text(
-                                taskTime == null ? '' : DateFormat('yyyy-MM-dd HH:mm').format(taskTime!),
-                                style: const TextStyle(color: Colors.blue, fontWeight: FontWeight.bold, fontSize: 18.0),
-                              ),
-                            ],
-                          ),
-                        )
+                            )
+                          ],
+                        ),
+                        const Text(
+                          "  Change",
+                          style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold, fontSize: 18.0),
+                        ),
                       ],
                     ),
-                    const Text(
-                      "  Change",
-                      style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold, fontSize: 18.0),
-                    ),
-                  ],
+                  ),
+                  color: Colors.white,
                 ),
-              ),
-              color: Colors.white,
-            ),
-            FloatingActionButton.extended(
-              icon: const Icon(Icons.save),
-              label: const Text('Save'),
-              onPressed: () {
-                apply(context);
-              },
-              shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(0.0)),
-            ),
-          ],
-        ));
+                ElevatedButton(
+                  child: const Text('Save'),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      apply(context);
+                    }
+                  },
+                ),
+              ],
+            )));
   }
 }
